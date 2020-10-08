952ab0536840a61d371f9f43cdf21b90f885ab3c
chfw <wangc_2011@hotmail.com>
2020-10-08 23:07:55 +0100

    :green_heart: udpate unit tests

--- tests/test_multiple_sheets.py
+++ tests/test_multiple_sheets.py
@@ -60,7 +60,7 @@ class TestAddBooks:
     def test_load_a_single_sheet3(self):
         pyexcel.get_book(file_name=self.testfile, sheet_index=10000)
 
-    @raises(ValueError)
+    @raises(KeyError)
     def test_load_a_single_sheet4(self):
         pyexcel.get_book(file_name=self.testfile, sheet_name="Not exist")
 
